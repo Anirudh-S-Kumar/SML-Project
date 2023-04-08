5dd6ab9f8c20fabe11f3cab80577577dde4728f0
Anirudh S. Kumar <anirudh.skumar.03@gmail.com>
2023-04-08 18:39:42 +0530

    made ensemble and dim reduction to accept multiple inputs

--- project.py
+++ project.py
@@ -12,8 +12,7 @@ test_data = pd.read_csv('data/test.csv')
 
 pipeline = mc.Pipeline(
     clustering_alg=None,
-    dim_reduction_alg_1="pca",
-    dim_reduction_alg_2="lda",
+    dim_reduction_algs=[("pca", 100)],
     outlier_detection_alg=None,
     classification_alg="logistic",
     ensemble_alg=None
@@ -23,4 +22,4 @@ print("Pipeline done")
 cv_scores = pipeline.cross_validate(X_t, y_t)
 print(f"Cross validation scores: {cv_scores}")
 
-pipeline.generate_submission(test_data)
+# pipeline.generate_submission(test_data)
