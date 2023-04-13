29bb106010181f14e483783dc5ff3fe8c8ae88cb
Anirudh S. Kumar <anirudh.skumar.03@gmail.com>
2023-04-13 22:20:41 +0530

    new pipeline in the works

--- new_pipeline.py
+++ new_pipeline.py
@@ -1,23 +1,41 @@
 from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.preprocessing import FunctionTransformer, OneHotEncoder
+from pipeline_components import *
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import BaggingClassifier, GradientBoostingClassifier
+from xgboost import XGBClassifier
+
+import numpy as np
+import pandas as pd
+import metaclass as mc
+import pipeline_components as pc
+
+train_data = pd.read_csv('data/train.csv')
+
+X_t = train_data.drop(['category', 'ID'], axis=1)
+y_t = train_data['category']
 
 
-class Pipeline:
 
-    def __init__(self,             clustering_alg: tuple[str, int], 
-            dim_reduction_algs: list[tuple[str, int]], 
-            outlier_detection_alg: str, 
-            classification_alg: str, 
-            ensemble_algs: list[str] = None):
-        self.clustering_alg = clustering_alg
-        self.dim_reduction_algs = dim_reduction_algs
-        self.outlier_detection_alg = outlier_detection_alg
-        self.classification_alg = classification_alg
-        self.ensemble_algs = ensemble_algs
+if __name__ == "__main__":
+    pipeline = Pipeline([
+        ("PCA 300", PCA(n_components=300)),
+        ("LDA 19", LinearDiscriminantAnalysis(n_components=19)),
+        ('Bagging', BaggingClassifier(base_estimator=MLPClassifier(activation='relu', 
+                                                                    solver='lbfgs', 
+                                                                    alpha=10, 
+                                                                    hidden_layer_sizes=(310), 
+                                                                    random_state=1,
+                                                                    max_iter=1000), 
+                                    n_estimators=10, random_state=1))
+      
+        ])
 
-        self.standardizer = None
-        self.cl = None
-        self.clus = None
-        self.dim_red_objs = []
-
-    
+    pipeline.fit(X_t, y_t)
+    print("Pipeline done")
+    cross_val_scores=cross_val_score(pipeline, X_t, y_t, cv=5)
+    print(cross_val_scores.mean(), cross_val_scores.std())
